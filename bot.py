6c5cac3c8316bedf7d5fecc45d62799b4fc3261e
Andrey Dementiev <andreydem42@gmail.com>
2025-11-07 23:51:31 +0300

    init

---
+++ bot.py
@@ -0,0 +1,132 @@
+import os
+from time import sleep
+import threading
+import telebot
+from prettytable import PrettyTable
+from datetime import datetime
+from pytz import timezone
+from dotenv import load_dotenv
+import atexit
+import requests
+import logging
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename='logs', filemode='w')
+logger = logging.getLogger(__name__)
+
+
+load_dotenv()
+TOKEN = os.getenv('BOT_TOKEN')
+CHAT_IDS = list(map(int, os.getenv('CHAT_IDS').split(',')))
+TIMEZONE = timezone('Europe/Moscow')
+bot = telebot.TeleBot(TOKEN, parse_mode='Markdown')
+last_updated = datetime.now()
+wallet_positions = {}
+
+
+with open('wallets.txt', 'r') as f:
+    for line in f:
+        wallet = line.strip()
+        if wallet:
+            wallet_positions[wallet] = []
+
+
+def add_wallet(chat_id, wallet):
+    if wallet not in wallet_positions:
+        wallet_positions[wallet] = []
+        send_everyone(f'Wallet {wallet} added')
+    else:
+        bot.send_message(chat_id, f'Wallet {wallet} is already being tracked')
+
+
+def remove_wallet(chat_id, wallet):
+    if wallet in wallet_positions:
+        del wallet_positions[wallet]
+        send_everyone(chat_id, f'Wallet {wallet} removed')
+    else:
+        bot.send_message(chat_id, f'Wallet {wallet} is not being tracked')
+
+
+@bot.message_handler(func=lambda m: True)
+def echo(m):
+    if m.chat.id not in CHAT_IDS:
+        return
+    if m.text.startswith('/addwallet '):
+        wallet = m.text.split(' ')[1]
+        add_wallet(m.chat.id, wallet)
+    elif m.text.startswith('/removewallet '):
+        wallet = m.text.split(' ')[1]
+        remove_wallet(m.chat.id, wallet)
+    else:
+        message = f'Last updated: {str(last_updated.astimezone(TIMEZONE))}'
+        bot.send_message(m.chat.id, message)
+
+
+def format_position(pos):
+    return {
+        'ticker': pos['position']['coin'],
+        'size': pos['position']['szi'],
+        'leverage': pos['position']['leverage']['value'],
+        'direction': 'Short' if pos['position']['szi'][0] == '-' else 'Long'
+    }
+
+
+def create_table(positions):
+    t = PrettyTable(['Ticker', 'Direction', 'Leverage', 'Size'])
+    for pos in positions:
+        t.add_row([pos['ticker'], pos['direction'], pos['leverage'], pos['size']])
+    t.align = 'l'
+    return t
+
+
+def send_everyone(message):
+    try:
+        for chat_id in CHAT_IDS:
+            bot.send_message(chat_id, message)
+    except Exception as e:
+        logger.error(f"exception while sending message: {e}")
+
+
+def worker():
+    global last_updated, wallet_positions
+    while True:
+        try:
+            for wallet, positions in wallet_positions.items():
+                payload = {
+                    "type": "clearinghouseState",
+                    "user": wallet
+                }
+                r = requests.post('https://api.hyperliquid.xyz/info', json=payload)
+                data = r.json()['assetPositions']
+                new_positions = list(map(format_position, data))
+                if positions == []:
+                    wallet_positions[wallet] = new_positions
+                elif new_positions != positions:
+                    t = create_table(new_positions)
+                    message = f'*Positions changed!*\n`{t}`\nhttps://hyperdash.info/trader/{wallet}'
+                    send_everyone(message)
+                    wallet_positions[wallet] = new_positions
+            last_updated = datetime.now()
+        except Exception as e:
+            logger.error(f"exception in worker: {e}")
+        finally:
+            sleep(5)
+
+
+def save_wallets():
+    try:
+        with open('wallets.txt', 'w') as f:
+            f.write('\n'.join(wallet_positions.keys()))
+    except Exception as e:
+        logger.error(f"exception while saving wallets: {e}")
+        sleep(5)
+
+
+atexit.register(save_wallets)
+threading.Thread(target=worker, daemon=True).start()
+while True:
+    try:
+        bot.infinity_polling(timeout=60, long_polling_timeout=60)
+    except Exception as e:
+        logger.error(f"exception while polling: {e}")
+        sleep(5)
